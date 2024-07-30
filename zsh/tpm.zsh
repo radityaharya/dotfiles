26bc09178002d649ada7e34582567486304598d4
Raditya Harya <radityaharya02@gmail.com>
2024-07-30 20:28:06 +0700

    update tpm instalation

--- zsh/tpm.zsh
+++ zsh/tpm.zsh
@@ -1,7 +1,7 @@
 install_tmux_plugin_manager() {
-  echo -e "${YELLOW}Installing tmux plugin manager...${NC}"
   mkdir -p ~/.tmux/plugins
   if [ ! -d ~/.tmux/plugins/tpm ]; then
+    echo -e "${YELLOW}Installing tmux plugin manager...${NC}"
     if git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm; then
       echo -e "${GREEN}Press prefix + I to install plugins${NC}"
     else
