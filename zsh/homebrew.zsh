b748dc07ed82d86d5f4280ff00f676cd5decea54
radityaharya <radityaharya02@gmail.com>
2024-09-18 17:41:12 +0700

    remove infisical

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -37,12 +37,12 @@ if ! command -v aichat &> /dev/null; then
   brew install aichat
 fi
 
-if ! command -v glow &> /dev/null; then
-  echo "glow not found. Installing..."
-  brew install glow
-fi
+# if ! command -v glow &> /dev/null; then
+#   echo "glow not found. Installing..."
+#   brew install glow
+# fi
 
-if ! command -v infisical &> /dev/null; then
-  echo "glow not found. Installing..."
-  brew install infisical/get-cli/infisical
-fi
+# if ! command -v infisical &> /dev/null; then
+#   echo "glow not found. Installing..."
+#   brew install infisical/get-cli/infisical
+# fi
